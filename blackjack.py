94ee9cf4c2e62304468362015adabc445d1ba6bf
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-04-23 12:08:40 -0400

    Start building out dealer functions.

--- blackjack.py
+++ blackjack.py
@@ -5,7 +5,8 @@ def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
 suits = ["Spades \u2664", "Hearts \u2661", "Clubs \u2667", "Diamonds \2662"]
-cards = {
+card_name: ["A", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+card_value = {
     "A": 11,
     "Two": 2,
     "Three": 3,
@@ -22,38 +23,35 @@ cards = {
 }
 
 class Card:
-    def __init__(self, suit, card):
+    def __init__(self, suit, card_name, card_value):
         self.suit = suit
-        self.card = card
+        self.card_name = card_name
+        self.card_value = card_value
 
 class Deck:
     def __init__(self):
-        deck = []
+        self.deck = []
         for suit in suits:
-            for card in cards:
-                deck.append(Card(suit, card))
-
-    def shuffle(self):
-        pass
+            for card in card_name:
+                self.deck.append(Card(suit, card, card_value[card]))
 
 class Dealer:
     def __init__(self):
-        dealer_hand = []
-        dealer_score = 0
+        self.dealer_hand = []
+        self.dealer_score = 0
 
-    def deal(self):
-        pass
-
-    def hit(self):
-        pass
+    def add_card(self):
+        added_card = random.choice(Deck.deck)
+        self.dealer_hand.append(added_card)
+        self.dealer_score += added_card.card_value
 
     def stand(self):
         pass
         
 class Player:
     def __init__(self):
-        player_hand = []
-        player_score = 0
+        self.player_hand = []
+        self.player_score = 0
 
     def hit(self):
         pass
@@ -69,5 +67,14 @@ class Blackjack:
         self.player = Player()
 
     def play_game(self):
-        action = input("Do you want to play a game of blackjack? Y/N ")
-
+        action = input("Do you want to play a game of blackjack? Y/N ").strip().lower()
+        if action == "y":
+            # Add card player
+            # Add card dealer
+            # Add card player
+            # Add card dealer
+            # Print "You have x & y in your hand. Your total is z." 
+            # Check for player blackjack.
+            # If blackjack...player wins
+            # If no blackjack: Do you want to hit or stand?"
+            pass
