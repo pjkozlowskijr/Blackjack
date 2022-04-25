57da74c2c68458328846dab4ad8e68a21f0fc287
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-04-25 13:36:43 -0400

    Fix bugs in placebet function and when player busts.

--- blackjack.py
+++ blackjack.py
@@ -49,18 +49,18 @@ class Betting():
         self.borrow_money = ""
 
     def placebet(self):
-        wager = input(f"\nYour current bank is: ${self.bank}. How much would you like to wager? ")
-        if type(wager) == int and wager > 0:
-            self.wager = wager
+        wager = input(f"Your current bank is: ${self.bank}. How much would you like to wager? ")
+        if int(wager) > 0:
+            self.wager = int(wager)
             clear_screen
             print(f"\nThis is where the fun begins. Let's see what you can do with the ${self.wager} you risked.")
-            if wager > self.bank:
+            if int(wager) > self.bank:
                 print(f"You only have ${self.bank} in your bank. Stop being broke and get more money if you wanna bet that much.")
         elif wager == "quit":
             self.wager = "quit"
         else:
             clear_screen()
-            print("\nInvalid entry. Please enter only whole numbers as digits.")
+            print("Invalid entry. Please enter only whole numbers as digits.\n")
             self.placebet()
        
     def losingbet(self):
@@ -113,6 +113,7 @@ class Blackjack:
         print("Welcome to Patrick & Kyle\'s blackjack table! Good luck!")
         help_or_play = input("\nTo start playing, enter 'play'. To see the help menu, type 'help'. ").strip().lower()
         if help_or_play == "play":
+            clear_screen()
             self.play_game()
         elif help_or_play == "help":
             self.help()
@@ -163,7 +164,6 @@ class Blackjack:
             self.check_dealer_score()
 
     def start_game(self):
-        clear_screen()
         self.player.add_card(self.deck.deal())
         self.dealer.add_card(self.deck.deal())
         self.player.add_card(self.deck.deal())
@@ -195,7 +195,9 @@ class Blackjack:
                         self.player.ace_adjust()
                         if self.player.score > 21:
                             print("You busted!")
+                            self.playing = False
                             self.betting.losingbet()
+                            self.play_again()
                         else:
                             print("\nYour Ace was adjusted from 11 points to 1 point, taking total score back under 21.")
                             self.show_cards()
@@ -211,7 +213,7 @@ class Blackjack:
                     print("Invalid entry. Try again.")
 
     def show_all_cards(self):
-        print("\nYour hand:")
+        print("Your hand:")
         for ind in range(len(self.player.hand)):
             print(f"{self.player.hand[ind].card_name} of {self.player.hand[ind].suit}")
         print("Your score:", self.player.score)
@@ -224,7 +226,7 @@ class Blackjack:
         if self.betting.borrow_money != "n" and self.betting.borrow_money != "quit":
             self.show_all_cards()
             if self.dealer.score < 17:
-                print("\nDealer takes card since score under 17.")
+                print("\nDealer takes card since score under 17.\n")
                 self.dealer.add_card(self.deck.deal())
                 self.check_dealer_score()
             elif self.dealer.score > 21:
@@ -255,6 +257,7 @@ class Blackjack:
     def play_again(self):
         again = input("\nDo you want to play again? Y/N ").strip().lower()
         if again == "y":
+            clear_screen()
             self.deck = Deck()
             self.deck.shuffle()
             self.dealer = Person()
